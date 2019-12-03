e9c7693867afa4ebec700ea42020c34fd46a944f
Faiz Zaman <faiz@Faiz-Debian-PC.home>
2019-12-03 16:05:10 +0000

    Got seeds from index table

--- fasta.py
+++ fasta.py
@@ -4,22 +4,43 @@ def heuralign(alphabet, substitution_matrix, seq1, seq2):
 
   # defining parameters
   ktup = 2
+  
+  index_table = get_index_table(ktup, seq1)
+  seeds = get_seeds(ktup, index_table, seq2)
+  print(index_table)
+  print(seeds)
+
+
+def get_index_table(ktup, seq1):
+
   index_table = {}
 
   # go through seq1 and store ktup strings in index table
-  for letter in range(0, len(seq1) - ktup + 1):      
-    match = seq1[letter]
+  for letter_index in range(0, len(seq1) - ktup + 1):      
+    match = seq1[letter_index]
     for i in range(1, ktup):
-      match += seq1[letter + i]
+      match += seq1[letter_index + i]
 
     if match in index_table:
-      index_table[match].append(letter)
+      index_table[match].append(letter_index)
     else:
-      index_table[match] = [letter]
+      index_table[match] = [letter_index]
 
-  print(index_table)
+  return index_table
 
 
+def get_seeds(ktup, index_table, seq2):
+
+  # go through seq2 and get seeds by matching to index table
+  seeds = []
+  for letter_index in range(0, len(seq2) - ktup + 1):
+      match = seq2[letter_index]
+      for i in range(1, ktup):
+        match += seq2[letter_index + i]
+      if match in index_table:
+        for seq1_position in index_table[match]:
+          seeds.append((seq1_position, letter_index))
+  return seeds
 
 
 
