2d19856e50588044dc772239b047bf7dfd029661
Faiz Zaman <faiz@Faiz-Debian-PC.home>
2019-12-03 16:10:19 +0000

    Made finding ktups more efficient and seperated into another function

--- fasta.py
+++ fasta.py
@@ -7,8 +7,7 @@ def heuralign(alphabet, substitution_matrix, seq1, seq2):
   
   index_table = get_index_table(ktup, seq1)
   seeds = get_seeds(ktup, index_table, seq2)
-  print(index_table)
-  print(seeds)
+  
 
 
 def get_index_table(ktup, seq1):
@@ -17,9 +16,7 @@ def get_index_table(ktup, seq1):
 
   # go through seq1 and store ktup strings in index table
   for letter_index in range(0, len(seq1) - ktup + 1):      
-    match = seq1[letter_index]
-    for i in range(1, ktup):
-      match += seq1[letter_index + i]
+    match = get_match(ktup, letter_index, seq1)
 
     if match in index_table:
       index_table[match].append(letter_index)
@@ -34,15 +31,20 @@ def get_seeds(ktup, index_table, seq2):
   # go through seq2 and get seeds by matching to index table
   seeds = []
   for letter_index in range(0, len(seq2) - ktup + 1):
-      match = seq2[letter_index]
-      for i in range(1, ktup):
-        match += seq2[letter_index + i]
+      match = get_match(ktup, letter_index, seq2)
+
       if match in index_table:
         for seq1_position in index_table[match]:
           seeds.append((seq1_position, letter_index))
+
   return seeds
 
 
+def get_match(ktup, letter_index, seq):
+
+  # get string of length ktup
+  match = seq[letter_index:letter_index + ktup]
+  return match
 
 alphabet = "ABC"
 substitution_matrix = [[2, -1, -1, -1, -2], [-1, 2, -1, -1, -2], [-1, -1, 2, -1, -2], [-1, -1, -1, 2, -2], [-1, -1, -1, -1, -2]]
